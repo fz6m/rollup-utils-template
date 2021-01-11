09f620ecc15b40af78ad3612bc0235906448aacb
fz6m <fz6meng@gmail.com>
2021-01-12 00:04:44 +0800

    feat: remove style components 🍑

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
+import React from 'react'
+
 // css module
 import style from './style.module.scss'
 
 // css file
 import '@/style.less'
 
-// style component
-import styled from '@emotion/styled'
-const Button = styled.button`
-  color: red;
-`
-
 // jsx
 export default (): JSX.Element => (
-  <Button className={style.module}>content</Button>
+  <button className={style.module}>content</button>
 )
